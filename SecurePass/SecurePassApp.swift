5baf214c23cd6a9961d7d54ed93eac527833db34
Aditi More <adit.more2001@gmail.com>
2025-11-27 22:02:03 -0800

    Initial Commit

---
+++ SecurePass/SecurePassApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurePassApp.swift
+//  SecurePass
+//
+//  Created by Aditi More on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SecurePassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
